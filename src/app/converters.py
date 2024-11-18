93473ad7cbe5a4a670ea8c2b7127329bb7d5651e
Rich Tebb <rich@tebb.io>
2024-11-18 11:13:08 +0000

    Add converter for model JSON

--- src/app/converters.py
+++ src/app/converters.py
@@ -1,5 +1,16 @@
 
 
+from types import NoneType
+from typing import Any
+
 from cattrs.preconf.json import make_converter
 
 converter = make_converter()
+
+@converter.register_structure_hook
+def json_null_hook(val: Any, _: Any) -> NoneType: # type: ignore[valid-type]
+    """
+    This hook will be registered for JSON nulls. These are needed so that an ApiResult can have no 'result', which is
+    represented by a JSON null in the received response.
+    """
+    return None
--- tests/app/test_converters.py
+++ tests/app/test_converters.py
@@ -1,4 +1,5 @@
 import json
+from types import NoneType
 from typing import Any
 
 import pytest
@@ -6,7 +7,18 @@ from cattrs.preconf.json import JsonConverter
 from utils import support_files_dir
 
 import app.converters as converters
-from app.models import ApiResult, Environment, GameMode, Layer, Orientation, Team
+from app.models import (
+    ApiResult,
+    ApiResultWithArgs,
+    DefaultMethods,
+    Environment,
+    GameMode,
+    Layer,
+    Orientation,
+    SetVotemapWhitelistParams,
+    Team,
+    VoteMapUserConfig,
+)
 
 SUPPORT_FILES_DIR = support_files_dir(__file__)
 
@@ -30,12 +42,12 @@ def describe_structure():
             }
             """
             contents = json.loads(json_content)
-            layers = converter.structure(contents, ApiResult[int])
-            assert layers.failed is False
-            assert layers.result == 5
-            assert layers.error is None
-            assert layers.version == "v10.5.1"
-            assert layers.command == "simple_test"
+            api_result = converter.structure(contents, ApiResult[int])
+            assert api_result.failed is False
+            assert api_result.result == 5
+            assert api_result.error is None
+            assert api_result.version == "v10.5.1"
+            assert api_result.command == "simple_test"
 
         def failed_result(converter: JsonConverter):
             json_content = """
@@ -50,10 +62,10 @@ def describe_structure():
             }
             """
             contents = json.loads(json_content)
-            layers = converter.structure(contents, ApiResult[int])
-            assert layers.failed is True
-            assert layers.result is None
-            assert layers.error == "Error message"
+            api_result = converter.structure(contents, ApiResult[int])
+            assert api_result.failed is True
+            assert api_result.result is None
+            assert api_result.error == "Error message"
 
     def describe_with_get_maps():
         @pytest.fixture
@@ -74,9 +86,9 @@ def describe_structure():
             assert len(layers.result) == 90
 
         def can_read_skirmish_map(converter: JsonConverter, contents: Any):
-            layers = converter.structure(contents, ApiResult[list[Layer]])
-            assert layers.result
-            layer = next(lyr for _, lyr in enumerate(layers.result) if lyr.id == "CAR_S_1944_Day_P_Skirmish")
+            api_result = converter.structure(contents, ApiResult[list[Layer]])
+            assert api_result.result
+            layer = next(lyr for _, lyr in enumerate(api_result.result) if lyr.id == "CAR_S_1944_Day_P_Skirmish")
             assert layer.game_mode == GameMode.CONTROL
             assert layer.attackers is None
             assert layer.map.id == "carentan"
@@ -86,9 +98,9 @@ def describe_structure():
             assert layer.map.axis.name == "ger"
 
         def can_read_offensive_map(converter: JsonConverter, contents: Any):
-            layers = converter.structure(contents, ApiResult[list[Layer]])
-            assert layers.result
-            layer = next(lyr for _, lyr in enumerate(layers.result) if lyr.id == "PHL_L_1944_OffensiveGER")
+            api_result = converter.structure(contents, ApiResult[list[Layer]])
+            assert api_result.result
+            layer = next(lyr for _, lyr in enumerate(api_result.result) if lyr.id == "PHL_L_1944_OffensiveGER")
             assert layer.game_mode == GameMode.OFFENSIVE
             assert layer.attackers == Team.AXIS
             assert layer.map.id == "purpleheartlane"
@@ -99,9 +111,9 @@ def describe_structure():
             assert layer.environment == Environment.DAY
 
         def can_read_warfare_map(converter: JsonConverter, contents: Any):
-            layers = converter.structure(contents, ApiResult[list[Layer]])
-            assert layers.result
-            layer = next(lyr for _, lyr in enumerate(layers.result) if lyr.id == "driel_warfare")
+            api_result = converter.structure(contents, ApiResult[list[Layer]])
+            assert api_result.result
+            layer = next(lyr for _, lyr in enumerate(api_result.result) if lyr.id == "driel_warfare")
             assert layer.game_mode == GameMode.WARFARE
             assert layer.attackers is None
             assert layer.map.id == "driel"
@@ -111,3 +123,49 @@ def describe_structure():
             assert layer.map.axis.name == "ger"
             assert layer.environment == Environment.DAY
             assert layer.pretty_name == "Driel Warfare"
+
+    def describe_with_get_votemap_config():
+        @pytest.fixture
+        def converter() -> JsonConverter:
+            return converters.converter
+
+        @pytest.fixture
+        def contents() -> Any:
+            filepath = SUPPORT_FILES_DIR.joinpath("get_votemap_config.json")
+            with filepath.open() as f:
+                contents = json.load(f)
+            return contents
+
+        def can_read_config(converter: JsonConverter, contents: Any):
+            api_result = converter.structure(contents, ApiResult[VoteMapUserConfig])
+            assert api_result.failed is False
+            assert api_result.result
+            assert api_result.result.allow_consecutive_offensives is False
+            assert api_result.result.allow_consecutive_offensives_opposite_sides is False
+            assert api_result.result.no_vote_text == "No votes recorded yet"
+            assert api_result.result.default_method == DefaultMethods.random_all_maps
+
+    def describe_with_set_votemap_whitelist():
+        @pytest.fixture
+        def converter() -> JsonConverter:
+            return converters.converter
+
+        @pytest.fixture
+        def contents() -> Any:
+            filepath = SUPPORT_FILES_DIR.joinpath("set_votemap_whitelist.json")
+            with filepath.open() as f:
+                contents = json.load(f)
+            return contents
+
+        def can_read_result(converter: JsonConverter, contents: Any):
+            api_result = converter.structure(contents, ApiResultWithArgs[NoneType, SetVotemapWhitelistParams])
+            assert api_result.failed is False
+            assert api_result.result is None
+            assert api_result.arguments.map_names == [
+                "stalingrad_warfare_night",
+                "carentan_warfare",
+                "mortain_offensiveUS_overcast",
+                "hurtgenforest_offensive_ger",
+                "PHL_L_1944_Warfare_Night",
+                "carentan_warfare_night",
+            ]
