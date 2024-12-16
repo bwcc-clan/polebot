b4ca368fbe3010f949d85a5c75d3d8e2e1c36915
Rich Tebb <rich@tebb.io>
2024-12-16 22:42:47 +0000

    Update exception formatting in log

--- src/app/logging_utils.py
+++ src/app/logging_utils.py
@@ -5,8 +5,22 @@ import os
 from queue import Queue
 
 
+class OneLineExceptionFormatter(logging.Formatter):
+    def formatException(self, exc_info) -> str:  # type: ignore[no-untyped-def]
+        result = super().formatException(exc_info)
+        return repr(result)  # or format into one line however you want to
+
+    def format(self, record: logging.LogRecord) -> str:
+        s = super().format(record)
+        if record.exc_text:
+            s = s.replace("\n", "") + "|"
+        else:
+            s = s.replace("\n", "\\n")
+        return s
+
+
 def configure_logger(log_path: str, log_level: str = "INFO") -> None:
-    q = Queue() # type: ignore
+    q = Queue()  # type: ignore
     os.makedirs(log_path, exist_ok=True)
     logfile = os.path.join(log_path, "votemapper.log")
     print(f"Logging to {logfile}")
@@ -15,6 +29,7 @@ def configure_logger(log_path: str, log_level: str = "INFO") -> None:
             "version": 1,
             "formatters": {
                 "default": {
+                    "()": "app.logging_utils.OneLineExceptionFormatter",
                     "format": "%(asctime)s (%(name)s) [%(levelname)s] %(message)s",
                     "datefmt": "%Y-%m-%dT%H:%M:%S",
                 }
@@ -51,6 +66,5 @@ def configure_logger(log_path: str, log_level: str = "INFO") -> None:
         }
     )
     qh = logging.getHandlerByName("queue_handler")
-    qh.listener.start() # type: ignore
+    qh.listener.start()  # type: ignore
     atexit.register(qh.listener.stop)  # type: ignore[union-attr]
-
